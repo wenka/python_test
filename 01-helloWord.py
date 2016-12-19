eb434b214f27dc5df194437dbfb759e1b07a5218
wenka <wenkaWK@outlook.com>
2016-12-19 13:28:43 +0800

    helloWorld
    HelloWorld。Python

---
+++ 01-helloWord.py
@@ -0,0 +1,26 @@
+#encoding=utf-8
+print("hello world!");
+print"Hello world！";
+
+if 9<5:
+    print "it is true!";
+else:
+    print "it is wrong!";
+
+    
+print "abcdefg"+ \
+"hijklmn!" 
+
+days = ['Monday', 'Tuesday',
+        'Wednesday','Thursday', 'Friday']
+print days;
+
+print '这是单引号'
+print "这是双引号"
+print """第一行
+第二行
+第三行
+这是三引号，可以包含多行"""
+
+
+raw_input("\n\n\nplease enter key to exit!")
